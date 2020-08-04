c6b262f8651567674eb39a07e20ccdf0e056bb8e
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-04 18:38:37 +0300

    Added Game entity

---
+++ GuessTheNumber/GuessTheNumber.DAL/Entities/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber.DAL.Entities
+{
+    public class Game: BaseEntity
+    {
+        public int OwnerId { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public int WinnerId { get; set; }
+    }
+}
