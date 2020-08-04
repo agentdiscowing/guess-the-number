54dd13cb4932c659f32e44c4ed22270285f3cd3f
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-04 18:36:23 +0300

    Added User entity

---
+++ GuessTheNumber/GuessTheNumber.DAL/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber.DAL.Entities
+{
+    public class User: BaseEntity
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
