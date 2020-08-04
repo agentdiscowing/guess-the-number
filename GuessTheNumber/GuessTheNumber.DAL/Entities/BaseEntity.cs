6c4c1428a7e074a679d55a2d8cf1bc9b772721d7
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-04 18:21:03 +0300

    Added BaseEntity to DAL

---
+++ GuessTheNumber/GuessTheNumber.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber.DAL.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
