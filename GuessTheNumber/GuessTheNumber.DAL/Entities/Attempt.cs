d8c4963825338ba568d82ecc586c30513a710ec6
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-04 18:39:02 +0300

    Added Attempt entity to store history

---
+++ GuessTheNumber/GuessTheNumber.DAL/Entities/Attempt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber.DAL.Entities
+{
+    public class Attempt
+    {
+        public int UserId { get; set; }
+
+        public int GameId { get; set; }
+
+        public int Number { get; set; }
+    }
+}
