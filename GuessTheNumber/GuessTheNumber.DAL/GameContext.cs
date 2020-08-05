66e0e00d7874f288a67ecaffc58332a51bb28f1f
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-05 13:01:23 +0300

    Added context for EF

---
+++ GuessTheNumber/GuessTheNumber.DAL/GameContext.cs
@@ -0,0 +1,38 @@
+﻿namespace GuessTheNumber.DAL
+{
+    using GuessTheNumber.DAL.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class GameContext : DbContext
+    {
+        public GameContext(DbContextOptions<GameContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attempt>().HasKey(q =>
+            new
+            {
+                q.GameId,
+                q.UserId,
+                q.Number
+            });
+
+            modelBuilder.Entity<Game>().HasOne(g => g.Owner).WithMany(u => u.Games).HasForeignKey(g => g.OwnerId);
+
+            modelBuilder.Entity<Game>().HasOne(g => g.Winner).WithMany().HasForeignKey(g => g.WinnerId);
+
+            modelBuilder.Entity<Attempt>().HasOne(a => a.User).WithMany().HasForeignKey(a => a.UserId);
+
+            modelBuilder.Entity<Attempt>().HasOne(a => a.Game).WithMany(g => g.Attempts).HasForeignKey(a => a.GameId);
+
+            modelBuilder.Entity<User>().HasIndex(u => new { u.Email, u.Username }).IsUnique();
+        }
+    }
+}
