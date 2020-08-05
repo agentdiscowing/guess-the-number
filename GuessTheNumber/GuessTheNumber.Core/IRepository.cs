0c74c1cecf49b7e627c9b690070f66d0b67bc4ed
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-05 13:13:52 +0300

    Added Find method to repository

--- GuessTheNumber/GuessTheNumber.Core/IRepository.cs
+++ GuessTheNumber/GuessTheNumber.Core/IRepository.cs
@@ -1,5 +1,8 @@
 ﻿namespace GuessTheNumber.Core
 {
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
 
     public interface IRepository<T>
@@ -8,5 +11,7 @@
         T Insert(T entity);
 
         Task SaveChangesAsync();
+
+        IQueryable<T> Find<TProperty>(Expression<Func<T, bool>> predicate, Expression<Func<T, TProperty>> includes);
     }
 }
