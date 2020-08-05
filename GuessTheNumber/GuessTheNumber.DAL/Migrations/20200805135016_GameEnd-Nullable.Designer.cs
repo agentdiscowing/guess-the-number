3ffb977cfbe982bcd4961805234a795c7b86391f
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-05 16:53:32 +0300

    Migrated changes

--- GuessTheNumber/GuessTheNumber.DAL/Migrations/20200805135016_GameEnd-Nullable.Designer.cs
+++ GuessTheNumber/GuessTheNumber.DAL/Migrations/20200805135016_GameEnd-Nullable.Designer.cs
@@ -4,14 +4,16 @@ using GuessTheNumber.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GuessTheNumber.DAL.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20200805135016_GameEnd-Nullable")]
+    partial class GameEndNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GuessTheNumber/GuessTheNumber.DAL/Migrations/20200805135016_GameEnd-Nullable.cs
@@ -0,0 +1,29 @@
+﻿namespace GuessTheNumber.DAL.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class GameEndNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Games",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Games",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
