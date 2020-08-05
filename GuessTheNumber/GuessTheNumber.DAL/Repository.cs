4b060174f1dbbec60dd27cc2f0c27145b2b5830a
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-05 13:01:48 +0300

    Implemented basic repository

---
+++ GuessTheNumber/GuessTheNumber.DAL/Repository.cs
@@ -0,0 +1,32 @@
+﻿namespace GuessTheNumber.DAL
+{
+    using System;
+    using System.Threading.Tasks;
+    using GuessTheNumber.Core;
+    using GuessTheNumber.DAL.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class Repository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        private readonly DbContext context;
+
+        private readonly DbSet<T> set;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+            this.set = context.Set<T>();
+        }
+
+        public T Insert(T entity)
+        {
+            return this.set.Add(entity).Entity;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+           await this.context.SaveChangesAsync();
+        }
+    }
+}
