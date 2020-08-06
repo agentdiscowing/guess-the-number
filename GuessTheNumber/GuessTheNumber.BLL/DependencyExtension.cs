c52d41ffa90924d7ac10cf5e2b4de9e9142e83e4
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-06 16:44:02 +0300

    Moved repository injection to BL

---
+++ GuessTheNumber/GuessTheNumber.BLL/DependencyExtension.cs
@@ -0,0 +1,22 @@
+﻿using GuessTheNumber.Core;
+using GuessTheNumber.DAL;
+using GuessTheNumber.Core.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuessTheNumber.BLL
+{
+    public static class DependencyExtension
+    {
+        public static IServiceCollection AddRepository<T>(this IServiceCollection services)
+            where T : BaseEntity
+        {
+            return services.AddScoped(typeof(IRepository<T>), typeof(Repository<T>));
+        }
+
+    }
+}
