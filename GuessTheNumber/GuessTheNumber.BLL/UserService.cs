689a2f71f80911b9be101f9c49df08418fa33a19
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-06 11:31:57 +0300

    Implemented login method

---
+++ GuessTheNumber/GuessTheNumber.BLL/UserService.cs
@@ -0,0 +1,37 @@
+﻿namespace GuessTheNumber.BLL
+{
+    using System;
+    using System.Linq;
+    using GuessTheNumber.BLL.Contracts;
+    using GuessTheNumber.BLL.Interfaces;
+    using GuessTheNumber.Core;
+    using GuessTheNumber.DAL.Entities;
+
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> userRepository;
+
+        public UserService(IRepository<User> userRepo)
+        {
+            this.userRepository = userRepo;
+        }
+
+        public bool Login(LoginUserContract creds)
+        {
+            var checkUser = this.userRepository.Find(u => u.Email == creds.Email).FirstOrDefault();
+
+            if (checkUser == null)
+            {
+                return false;
+            }
+
+            // will add hasing later
+            return checkUser.PasswordHash == creds.Password;
+        }
+
+        public LoginUserContract Register(NewUserContract newUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
