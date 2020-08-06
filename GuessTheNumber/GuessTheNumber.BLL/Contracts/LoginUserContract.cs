ff63ed809f2276188d9e6eb813175d30dea71e69
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-06 14:48:02 +0300

    Added validation

--- GuessTheNumber/GuessTheNumber.BLL/Contracts/LoginUserContract.cs
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/LoginUserContract.cs
@@ -1,9 +1,15 @@
 ﻿namespace GuessTheNumber.BLL.Contracts
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class LoginUserContract
     {
+        [Required]
+        [RegularExpression(@"\w+@[A-z]+\.([A-z]){2,4}")]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "Password must contain from 6 to 60 characters")]
         public string Password { get; set; }
     }
 }
