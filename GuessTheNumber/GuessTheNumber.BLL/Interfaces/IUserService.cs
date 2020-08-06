76015dfa8b05ae760f9c36bfacc3a4bfd42f1f0b
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-06 11:16:56 +0300

    Added UserService interface

---
+++ GuessTheNumber/GuessTheNumber.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using GuessTheNumber.BLL.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        bool Login(LoginUserContract creds);
+
+        LoginUserContract Register(NewUserContract newUser);
+    }
+}
