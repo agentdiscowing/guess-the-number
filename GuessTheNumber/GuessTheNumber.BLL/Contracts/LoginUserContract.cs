0d5eb1a3094ebb893ae48be6917afc2b13edc2f8
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-06 11:08:00 +0300

    Added Login contract

---
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/LoginUserContract.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessTheNumber.BLL.Contracts
+{
+    public class LoginUserContract
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
