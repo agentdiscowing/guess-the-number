0ce5f45af68287c11d8cc5f68bfc9597a17e004e
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-07 14:47:38 +0300

    Added ShortGameInfo contract

---
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/ShortGameInfoContract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumber.BLL.Contracts
+{
+    public class ShortGameInfoContract
+    {
+        public int Id { get; set; }
+
+        public string OwnerUsername { get; set; }
+    }
+}
