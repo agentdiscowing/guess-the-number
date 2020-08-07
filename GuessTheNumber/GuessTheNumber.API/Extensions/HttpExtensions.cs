75ab4559e63130aa9a8c4a742212260ada12516c
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-07 13:59:58 +0300

    Added extension method for getting user id

---
+++ GuessTheNumber/GuessTheNumber.API/Extensions/HttpExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace GuessTheNumber.API.Extensions
+{
+    using Microsoft.AspNetCore.Http;
+
+    public static class HttpExtensions
+    {
+        public static int? GetUserId(this HttpContext httpContext)
+        {
+            if (httpContext.User == null)
+            {
+                return null;
+            }
+
+            return int.Parse(httpContext.User.FindFirst("id").Value);
+        }
+    }
+}
