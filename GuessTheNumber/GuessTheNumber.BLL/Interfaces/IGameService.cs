cdf71953b34bc6837ba3d5cae41f965ff89b0e98
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-07 16:20:43 +0300

    Renamed EndGame method

--- GuessTheNumber/GuessTheNumber.BLL/Interfaces/IGameService.cs
+++ GuessTheNumber/GuessTheNumber.BLL/Interfaces/IGameService.cs
@@ -1,23 +1,15 @@
-﻿using GuessTheNumber.BLL.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GuessTheNumber.BLL.Interfaces
+﻿namespace GuessTheNumber.BLL.Interfaces
 {
+    using GuessTheNumber.BLL.Contracts;
+
     public interface IGameService
     {
-        // if game is already started return null, else return the number of the game
         int? StartGame(int userId, int number);
 
-        // if user is not entitled to end the game return false, else true (manually or on log off)
-        // if the game was forced to end then there is no winner
-        bool EndGame(int userId, int? winnerId = null);
-
-        // return -1 if number if less than was set, 0 if it is right, 1 if it's more than was set
         GameAttemptResults MakeAttempt(int userId, int number);
 
-        // check if there is a game going on rn
-        ShortGameInfoContract GetActiveGame(int userId);
+        ShortGameInfoContract GetActiveGame();
+
+        bool ForceEndGame(int userId);
     }
 }
