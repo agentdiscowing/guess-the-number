1da65cccd62358091a2b4fd41e631e60c5472e28
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-10 16:21:59 +0300

    Added Exception Filter

---
+++ GuessTheNumber/GuessTheNumber.Web/Filters/ExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿namespace GuessTheNumber.Web.Filters
+{
+    using GuessTheNumber.Core.Exceptions;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            int status = 500;
+
+            string message = string.Empty;
+
+            if (exception is GuessTheNumberException ex)
+            {
+                status = ex.Code;
+
+                message = ex.Message;
+            }
+
+            context.Result = new ContentResult
+            {
+                Content = message,
+                StatusCode = status
+            };
+        }
+    }
+}
