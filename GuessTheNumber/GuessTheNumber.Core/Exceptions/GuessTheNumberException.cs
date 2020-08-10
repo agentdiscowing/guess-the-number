dbb80b646a3ca6860cb8a72b9a24ac8b5e714980
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-10 11:10:03 +0300

    Defined custom exception

---
+++ GuessTheNumber/GuessTheNumber.Core/Exceptions/GuessTheNumberException.cs
@@ -0,0 +1,32 @@
+﻿namespace GuessTheNumber.Core.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+    using GuessTheNumber.Core.Constants;
+    using GuessTheNumber.Core.Enums;
+
+    public class GuessTheNumberException : Exception
+    {
+        public GuessTheNumberException()
+        : base(ExceptionMessages.GuessTheNumberException)
+        {
+        }
+
+        public GuessTheNumberException(string message)
+            : base(message)
+        {
+        }
+
+        public GuessTheNumberException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected GuessTheNumberException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public virtual int Code => (int)ExceptionEnums.Global.CustomException;
+    }
+}
