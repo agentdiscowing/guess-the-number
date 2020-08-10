28bae52c4aa73a3b6137794639485bbffd8690f0
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-10 11:09:35 +0300

    Added class for constant exception messages

---
+++ GuessTheNumber/GuessTheNumber.Core/Constants/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace GuessTheNumber.Core.Constants
+{
+    public static class ExceptionMessages
+    {
+        public static string GuessTheNumberException { get; private set; } = "Application logic failed. See the inner exception for more details.";
+    }
+}
