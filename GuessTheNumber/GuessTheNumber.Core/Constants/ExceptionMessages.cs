b2cd85c39bf51c0752c804ffb8aa4a88ede513a9
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-10 11:35:30 +0300

    Added UserDoesNotExist exception

--- GuessTheNumber/GuessTheNumber.Core/Constants/ExceptionMessages.cs
+++ GuessTheNumber/GuessTheNumber.Core/Constants/ExceptionMessages.cs
@@ -5,5 +5,7 @@
         public static string GuessTheNumberException { get; private set; } = "Application logic failed. See the inner exception for more details.";
 
         public static string GuessTheNumberEmailAlreadyExistsException { get; private set; } = "User with this email is already registered on the app";
+
+        public static string GuessTheNumberUserDoesNotExistException { get; private set; } = "User with this username or email is not registered on the app.";
     }
 }
---
+++ GuessTheNumber/GuessTheNumber.Core/Exceptions/GuessTheNumberUserDoesNotExistException.cs
@@ -0,0 +1,32 @@
+﻿namespace GuessTheNumber.Core.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+    using GuessTheNumber.Core.Constants;
+    using GuessTheNumber.Core.Enums;
+
+    public class GuessTheNumberUserDoesNotExistException : GuessTheNumberException
+    {
+        public GuessTheNumberUserDoesNotExistException()
+        : base(ExceptionMessages.GuessTheNumberUserDoesNotExistException)
+        {
+        }
+
+        public GuessTheNumberUserDoesNotExistException(string message)
+            : base(message)
+        {
+        }
+
+        public GuessTheNumberUserDoesNotExistException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected GuessTheNumberUserDoesNotExistException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public override int Code => (int)ExceptionEnums.Authentication.UserDoesNotExist;
+    }
+}
