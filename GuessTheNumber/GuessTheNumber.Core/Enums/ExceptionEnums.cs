90f9b91b1a4c7aaee07dbbc6e15532e71fc2b64d
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-10 11:08:52 +0300

    Added enum with exception codes

---
+++ GuessTheNumber/GuessTheNumber.Core/Enums/ExceptionEnums.cs
@@ -0,0 +1,19 @@
+﻿namespace GuessTheNumber.Core.Enums
+{
+    public class ExceptionEnums
+    {
+        public enum Global
+        {
+            CustomException = 1000
+        }
+
+        public enum Authentication
+        {
+            EmailAlreadyExists = 1101,
+            UsernameAlreadyExists = 1102,
+            InvalidPassword = 1103,
+            UserDoesNottExist = 1104,
+            UserWasNotCreated = 1105
+        }
+    }
+}
