eefa487056e98e5b153c5855ca44a7568d546e41
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-12 00:29:30 +0300

    Replaced properties for ErrorMessages with consts

--- GuessTheNumber/GuessTheNumber.Core/Constants/ErrorMessages.cs
+++ GuessTheNumber/GuessTheNumber.Core/Constants/ErrorMessages.cs
@@ -2,20 +2,20 @@
 {
     public static class ErrorMessages
     {
-        public static string ValidationError { get; private set; } = "One or more validation errors occured";
+        public const string ValidationError = "One or more validation errors occured";
 
-        public static string UnhandledException { get; private set; } = "Sorry, something went wrong on the server side. Try again later";
+        public const string UnhandledException = "Sorry, something went wrong on the server side. Try again later";
 
-        public static string GuessTheNumberException { get; private set; } = "Application logic failed.";
+        public const string GuessTheNumberException = "Application logic failed.";
 
-        public static string GuessTheNumberEmailAlreadyExistsException { get; private set; } = "User with this email is already registered on the app";
+        public const string GuessTheNumberEmailAlreadyExistsException = "User with this email is already registered on the app";
 
-        public static string GuessTheNumberUserDoesNotExistException { get; private set; } = "User with this username or email is not registered on the app.";
+        public const string GuessTheNumberUserDoesNotExistException = "User with this username or email is not registered on the app.";
 
-        public static string GuessTheNumberInvalidPasswordException { get; private set; } = "Invalid password was entered";
+        public const string GuessTheNumberInvalidPasswordException = "Invalid password was entered";
 
-        public static string GuessTheNumberUsernameAlreadyExistsException { get; private set; } = "User with this email is already registered on the app";
+        public const string GuessTheNumberUsernameAlreadyExistsException = "User with this email is already registered on the app";
 
-        public static string GuessTheNumberUserWasNotCreatedException { get; private set; } = "User was not registrered on the app";
+        public const string GuessTheNumberUserWasNotCreatedException = "User was not registrered on the app";
     }
 }
