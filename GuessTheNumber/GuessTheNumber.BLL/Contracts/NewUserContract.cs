46f85e6c42b5d0ace389814af9a219d1efc84c1f
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-11 11:51:53 +0300

    Removed validation from contract

--- GuessTheNumber/GuessTheNumber.BLL/Contracts/NewUserContract.cs
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/NewUserContract.cs
@@ -1,24 +1,11 @@
 ﻿namespace GuessTheNumber.BLL.Contracts
 {
-    using System.ComponentModel.DataAnnotations;
-
     public class NewUserContract
     {
-        [Required]
-        [StringLength(60, MinimumLength = 5, ErrorMessage = "Username must contain from 6 to 60 characters")]
         public string Username { get; set; }
 
-        [Required]
-        [RegularExpression(@"\w+@[A-z]+\.([A-z]){2,4}")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(60, MinimumLength = 5, ErrorMessage = "Password must contain from 6 to 60 characters")]
         public string Password { get; set; }
-
-        [Required]
-        [StringLength(60, MinimumLength = 5, ErrorMessage = "Password must contain from 6 to 60 characters")]
-        [Compare("Password", ErrorMessage = "Passwords don't match")]
-        public string ConfirmPassword { get; set; }
     }
 }
