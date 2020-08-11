d10c15684c4382accb6152e2e48fe1ef04dd22ef
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-11 19:22:33 +0300

    Added AuthSuccessResponse

---
+++ GuessTheNumber/GuessTheNumber.Web/Models/Response/AuthSuccessResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace GuessTheNumber.Web.Models.Response
+{
+    public class AuthSuccessResponse
+    {
+        public AuthSuccessResponse(string token)
+        {
+            this.Token = token;
+        }
+
+        public string Token { get; set; }
+    }
+}
