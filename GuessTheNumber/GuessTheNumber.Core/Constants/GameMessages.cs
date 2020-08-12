83e668ae627e2c608e8630ee41add1131376a0bd
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-12 14:12:18 +0300

    Added constant messages for attempt results'

---
+++ GuessTheNumber/GuessTheNumber.Core/Constants/GameMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace GuessTheNumber.Core.Constants
+{
+    public static class GameMessages
+    {
+        public const string GuessIsLessThanNumber = "Your number is less than needed.";
+
+        public const string GuessIsGreaterThanNumber = "Your number is greater than needed.";
+
+        public const string GuessIsCorrect = "Your guess is correct. Game is over";
+    }
+}
