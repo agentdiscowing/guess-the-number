08aa7ff565d7b88f0cb63126c49537774914298a
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-12 14:17:18 +0300

    Added response for attempt

---
+++ GuessTheNumber/GuessTheNumber.Web/Models/Response/GuessResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessTheNumber.Web.Models.Response
+{
+    public class GuessResponse
+    {
+        public int Number { get; set; }
+
+        public string Result { get; set; }
+    }
+}
