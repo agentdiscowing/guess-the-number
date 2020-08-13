bcd0a0efe4c5626e6c9ca3ed10f796d81ed791a5
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-14 02:00:24 +0300

    Added converter for game state

---
+++ GuessTheNumber/GuessTheNumber.Web/Extensions/ConvertingExtensions/GameStateToResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace GuessTheNumber.Web.Extensions.ConvertingExtensions
+{
+    using GuessTheNumber.Core.Resources;
+    using static GuessTheNumber.Core.Enums.GameLogicEnums;
+
+    public static partial class ConvertingExtensions
+    {
+        public static string ToResponse(this GameStates gameState)
+        {
+            string message = gameState switch
+            {
+                GameStates.ACTIVE => GameMessages.GameIsActive,
+                GameStates.WON => GameMessages.GameIsWon,
+                GameStates.OVER => GameMessages.GameIsOver
+            };
+            return message;
+        }
+    }
+}
