e747accd3f645141c2679307863ee43af2fbf52c
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-13 13:26:51 +0300

    Added IdentityContext for user management

---
+++ GuessTheNumber/GuessTheNumber.Web/Data/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace GuessTheNumber.Web.Data
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
