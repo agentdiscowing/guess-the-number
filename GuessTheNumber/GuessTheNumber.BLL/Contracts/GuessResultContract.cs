78fcbdb3534fbc1d71cea0f70aca5ebc38352d97
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-14 02:02:49 +0300

    Re-added GuessResult contract

---
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/GuessResultContract.cs
@@ -0,0 +1,11 @@
+﻿namespace GuessTheNumber.BLL.Contracts
+{
+    using static GuessTheNumber.Core.Enums.GameLogicEnums;
+
+    public class GuessResultContract
+    {
+        public int Number { get; set; }
+
+        public GameAttemptResults Result { get; set; }
+    }
+}
