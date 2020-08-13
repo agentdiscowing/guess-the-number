7753834adc06db455900d2c64160cf555b5a5488
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-13 21:04:09 +0300

    Added identity model builder

--- GuessTheNumber/GuessTheNumber.DAL/GameContext.cs
+++ GuessTheNumber/GuessTheNumber.DAL/GameContext.cs
@@ -1,21 +1,22 @@
 ﻿namespace GuessTheNumber.DAL
 {
-    using GuessTheNumber.Core.Entities;
+    using GuessTheNumber.DAL.Entities;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
-    public class GameContext : DbContext
+    public class GameContext : IdentityDbContext
     {
         public GameContext(DbContextOptions<GameContext> options)
             : base(options)
         {
         }
 
-        public DbSet<User> Users { get; set; }
-
         public DbSet<Game> Games { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Attempt>().HasKey(q =>
             new
             {
@@ -24,15 +25,13 @@
                 q.Number
             });
 
-            modelBuilder.Entity<Game>().HasOne(g => g.Owner).WithMany(u => u.Games).HasForeignKey(g => g.OwnerId);
+            modelBuilder.Entity<Game>().HasOne(g => g.Owner).WithMany().HasForeignKey(g => g.OwnerId);
 
             modelBuilder.Entity<Game>().HasOne(g => g.Winner).WithMany().HasForeignKey(g => g.WinnerId);
 
             modelBuilder.Entity<Attempt>().HasOne(a => a.User).WithMany().HasForeignKey(a => a.UserId);
 
             modelBuilder.Entity<Attempt>().HasOne(a => a.Game).WithMany(g => g.Attempts).HasForeignKey(a => a.GameId);
-
-            modelBuilder.Entity<User>().HasIndex(u => new { u.Email, u.Username }).IsUnique();
         }
     }
 }
