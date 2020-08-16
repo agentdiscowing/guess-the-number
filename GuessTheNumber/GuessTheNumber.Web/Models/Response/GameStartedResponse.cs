7e72c5f7abd0a7b12c58ee71ffc7b4ee62193028
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-16 17:59:55 +0300

    Added response for starting new game

---
+++ GuessTheNumber/GuessTheNumber.Web/Models/Response/GameStartedResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber.Web.Models.Response
+{
+    public class GameStartedResponse
+    {
+        public GameStartedResponse(int number)
+        {
+            this.Number = number;
+        }
+
+        public int Number { get; set; }
+
+        public string Message => $"Game with number {this.Number} is started";
+    }
+}
