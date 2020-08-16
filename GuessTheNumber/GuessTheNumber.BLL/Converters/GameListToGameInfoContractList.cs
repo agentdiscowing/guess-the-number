126466aa2028ae39c214a17042b83966396e1f73
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-16 16:28:49 +0300

    Added converters

---
+++ GuessTheNumber/GuessTheNumber.BLL/Converters/GameListToGameInfoContractList.cs
@@ -0,0 +1,16 @@
+﻿namespace GuessTheNumber.BLL.Converters
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using GuessTheNumber.BLL.Contracts;
+    using GuessTheNumber.DAL.Entities;
+
+    public static partial class Converters
+    {
+        public static IList<GameInfoContract> ToContractList(this IEnumerable<Game> games)
+        {
+            return games.Select(g => g.ToContract()).ToList();
+        }
+
+    }
+}
---
+++ GuessTheNumber/GuessTheNumber.BLL/Converters/GameToGameInfoContract.cs
@@ -0,0 +1,21 @@
+﻿namespace GuessTheNumber.BLL.Converters
+{
+    using System.Linq;
+    using GuessTheNumber.BLL.Contracts;
+    using GuessTheNumber.DAL.Entities;
+
+    public static partial class Converters
+    {
+        public static GameInfoContract ToContract(this Game game)
+        {
+            return new GameInfoContract
+            {
+                Id = game.Id,
+                Length = game.EndTime.Value.Subtract(game.StartTime),
+                Number = game.Number,
+                ParticipatsIds = game.Attempts.Select(a => a.UserId).Distinct().ToArray(),
+                WinnerId = game.WinnerId
+            };
+        }
+    }
+}
