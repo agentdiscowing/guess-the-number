a2d0a72cbd2b5f3a580e36602914f1c003262666
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-16 16:29:38 +0300

    Replaced manual converting in service with automatic converter

--- GuessTheNumber/GuessTheNumber.BLL/Services/HistoryService.cs
+++ GuessTheNumber/GuessTheNumber.BLL/Services/HistoryService.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using GuessTheNumber.BLL.Contracts;
+    using GuessTheNumber.BLL.Converters;
     using GuessTheNumber.BLL.Interfaces;
     using GuessTheNumber.DAL;
     using GuessTheNumber.DAL.Entities;
@@ -18,15 +19,13 @@
 
         public IList<GameInfoContract> GetGameHistory(int page, int gamesPerPage)
         {
-            var wonGames = this.gameRepository.Find(g => g.WinnerId != null).Skip(--page * gamesPerPage).Take(gamesPerPage).OrderBy(g => g.StartTime);
+            var wonGames = this.gameRepository.Find(g => g.WinnerId != null)
+                                              .Skip(--page * gamesPerPage)
+                                              .Take(gamesPerPage)
+                                              .OrderBy(g => g.StartTime)
+                                              .ToList();
 
-            return wonGames.Select(g => new GameInfoContract
-            {
-                Length = g.EndTime.Value.Subtract(g.StartTime),
-                Number = g.Number,
-                ParticipatsIds = g.Attempts.Select(a => a.UserId).ToArray(),
-                WinnerId = g.WinnerId
-            }).ToList();
+            return wonGames.ToContractList();
         }
     }
 }
