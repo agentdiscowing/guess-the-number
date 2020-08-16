034c16733ddec428f1966cdc97a114bd47b49c03
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-16 14:13:30 +0300

    Added history controller

---
+++ GuessTheNumber/GuessTheNumber.Web/Controllers/HistoryController.cs
@@ -0,0 +1,27 @@
+﻿namespace GuessTheNumber.Web.Controllers
+{
+    using GuessTheNumber.BLL.Interfaces;
+    using GuessTheNumber.Web.Extensions;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class HistoryController : ControllerBase
+    {
+        private readonly IGameService gameService;
+
+        public HistoryController(IGameService gameService)
+        {
+            this.gameService = gameService;
+        }
+
+        [HttpGet("history/{page}")]
+        public IActionResult GetGameHistory(int page, [FromBody] int gamesPerPage)
+        {
+            var gameList = this.gameService.GetGameHistory(page, gamesPerPage, this.HttpContext.GetUserId());
+            return Ok(gameList);
+        }
+    }
+}
