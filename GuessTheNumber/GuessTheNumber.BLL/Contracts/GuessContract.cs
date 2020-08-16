a62ce39563a87bd5fbdc81f59efc6acda44b387c
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-16 17:16:52 +0300

    Added guess contract

---
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/GuessContract.cs
@@ -0,0 +1,11 @@
+﻿namespace GuessTheNumber.BLL.Contracts
+{
+    public class GuessContract
+    {
+        public string UserId { get; set; }
+
+        public int Number { get; set; }
+
+        public string Username { get; set; }
+    }
+}
