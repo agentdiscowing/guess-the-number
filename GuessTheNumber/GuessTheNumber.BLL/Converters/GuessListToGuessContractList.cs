2aa46a0ccb29eeae69e5292169d9dc45fd2e00cc
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-16 17:17:20 +0300

    Added converter for Guess

---
+++ GuessTheNumber/GuessTheNumber.BLL/Converters/GuessListToGuessContractList.cs
@@ -0,0 +1,15 @@
+﻿namespace GuessTheNumber.BLL.Converters
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using GuessTheNumber.BLL.Contracts;
+    using GuessTheNumber.DAL.Entities;
+
+    public static partial class Converters
+    {
+        public static IList<GuessContract> ToContractList(this IEnumerable<Guess> guesses)
+        {
+            return guesses.Select(g => g.ToContract()).ToList();
+        }
+    }
+}
---
+++ GuessTheNumber/GuessTheNumber.BLL/Converters/GuessToGuessContract.cs
@@ -0,0 +1,18 @@
+﻿namespace GuessTheNumber.BLL.Converters
+{
+    using GuessTheNumber.BLL.Contracts;
+    using GuessTheNumber.DAL.Entities;
+
+    public static partial class Converters
+    {
+        public static GuessContract ToContract(this Guess guess)
+        {
+            return new GuessContract
+            {
+                UserId = guess.UserId,
+                Number = guess.Number,
+                Username = guess.User.UserName
+            };
+        }
+    }
+}
