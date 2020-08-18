b54cdf49993bd4ffb29374e2dd974d0b95fc92b3
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-18 15:51:33 +0300

    Added interceptors providers

--- guess-the-number/src/app/app.module.ts
+++ guess-the-number/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AuthModule } from './auth/auth.module'
 import { AppComponent } from './app.component';
+import { httpInterceptorProviders } from './http-interceptors';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AuthModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
