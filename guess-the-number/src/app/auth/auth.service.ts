e9b38898d15e519982aed0e2a2bb0c0a1c6760b4
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-18 15:45:58 +0300

    Added auth service with login method

---
+++ guess-the-number/src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { AuthResult } from './authResult';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private authUrl = 'https://localhost:5001/api/Account';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) : Observable<AuthResult>{
+    let url = this.authUrl + "/login";
+    return this.http.post<AuthResult>(url, {email, password}, this.httpOptions).pipe(
+      tap({ next: (x) => { 
+        this.setSession(x) 
+      }})
+    );
+  }
+
+  private setSession(authResult: AuthResult) {
+    localStorage.setItem('id_token', authResult.token);
+  }
+}
