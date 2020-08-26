06cdd73e8bfcfad4821699269381843e4ec22b11
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-26 13:37:29 +0300

    Added error component

--- GuessTheNumber/GuessTheNumber.Web/client/src/app/shared/index.ts
+++ GuessTheNumber/GuessTheNumber.Web/client/src/app/shared/index.ts
@@ -1,2 +1,5 @@
 export { SwitcherComponent } from './switcher/switcher.component';
 export { SwitcherModule } from './switcher/switcher.module';
+
+export {ErrorComponent} from './error/error.component';
+export {ErrorModule} from './error/error.module';
