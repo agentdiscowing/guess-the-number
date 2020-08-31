a508727e65c54aaa68f7f073b041a1b69baea280
Lolita Hevondian <agentdiscowing@gmail.com>
2020-08-31 14:05:12 +0300

    Implemented game service

--- GuessTheNumber/GuessTheNumber.Web/client/src/app/game/game.service.ts
+++ GuessTheNumber/GuessTheNumber.Web/client/src/app/game/game.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Game } from './gameplay/game';
+import { Game } from './gameplay/models/game';
+import { GameStartedResponse } from './gameplay/models/gameStartedResponse';
+import { GuessResult } from './gameplay/models/guessResult';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -11,7 +13,7 @@ export class GameService {
 
   private gameUrl = 'api/Game';  // URL to web api
 
-  private currentGame: Game;
+  public currentGameState: Game;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -23,8 +25,26 @@ export class GameService {
     let url = this.gameUrl + '/state';
     return this.http.get<Game>(url, this.httpOptions).pipe(
       tap({ next: (x) => { 
-         this.currentGame.state = x.state
+         this.currentGameState = x
       }})
+    );
+  }
+
+  sendGuess(number: number): Observable<GuessResult> {
+    let url = this.gameUrl + `/guess/${number}`;
+    return this.http.post<GuessResult>(url, this.httpOptions).pipe(
+      tap({ next: (x) => { 
+        this.getState()
+     }})
+    );;
+  }
+
+  startGame(number: number): Observable<GameStartedResponse> {
+    let url = this.gameUrl + `/start/${number}`;
+    return this.http.post<GameStartedResponse>(url, this.httpOptions).pipe(
+      tap({ next: (x) => { 
+        this.getState()
+     }})
     );
   }
 }
