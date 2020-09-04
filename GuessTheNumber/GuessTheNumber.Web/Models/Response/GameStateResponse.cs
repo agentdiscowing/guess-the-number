f3b63cb03f01e9188bb95d81ed03e5b49c62c834
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-04 13:42:11 +0300

    Added owner flag

--- GuessTheNumber/GuessTheNumber.Web/Models/Response/GameStateResponse.cs
+++ GuessTheNumber/GuessTheNumber.Web/Models/Response/GameStateResponse.cs
@@ -2,14 +2,17 @@
 {
     public class GameStateResponse
     {
-        public GameStateResponse(string message, string state)
+        public GameStateResponse(string message, string state, bool isOwner)
         {
             this.Message = message;
             this.State = state;
+            this.IsOwner = isOwner;
         }
 
         public string Message { get; set; }
 
         public string State { get; set; }
+
+        public bool IsOwner { get; set; }
     }
 }
