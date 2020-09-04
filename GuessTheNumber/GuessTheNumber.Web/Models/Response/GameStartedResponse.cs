9ea7bbd61412b11c90178b5288996c1b48dc8e80
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-04 13:41:59 +0300

    Added owner property

--- GuessTheNumber/GuessTheNumber.Web/Models/Response/GameStartedResponse.cs
+++ GuessTheNumber/GuessTheNumber.Web/Models/Response/GameStartedResponse.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GuessTheNumber.Web.Models.Response
+﻿namespace GuessTheNumber.Web.Models.Response
 {
     public class GameStartedResponse
     {
-        public GameStartedResponse(int number)
+        public GameStartedResponse(int number, string owner)
         {
             this.Number = number;
+            this.OwnerUsername = owner;
         }
 
         public int Number { get; set; }
+
+        public string OwnerUsername { get; set; }
 
         public string Message => $"Game with number {this.Number} is started";
     }
