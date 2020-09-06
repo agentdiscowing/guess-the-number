1841cb5e3e69b5c8819b14a842e7ec629cb95174
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-06 22:24:00 +0300

    Added comment service interface

---
+++ GuessTheNumber/GuessTheNumber.BLL/Interfaces/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿namespace GuessTheNumber.BLL.Interfaces
+{
+    using GuessTheNumber.BLL.Contracts;
+    using System.Collections.Generic;
+
+    public interface ICommentService
+    {
+        int SendComment(CommentContract comment);
+
+        void DeleteComment(int commentId, string username);
+
+        void EditComment(int commentId, string username, string newContent);
+
+        IEnumerable<CommentContract> GetComments();
+    }
+}
