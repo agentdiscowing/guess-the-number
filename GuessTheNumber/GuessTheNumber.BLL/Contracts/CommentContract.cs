2798e966fd8f0e9debfd3f00af8a5d8df0d2e0e2
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-06 22:26:44 +0300

    Added contract for comment

---
+++ GuessTheNumber/GuessTheNumber.BLL/Contracts/CommentContract.cs
@@ -0,0 +1,15 @@
+﻿namespace GuessTheNumber.BLL.Contracts
+{
+    using System;
+
+    public class CommentContract
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime PostDate { get; set; }
+
+        public string Text { get; set; }
+    }
+}
