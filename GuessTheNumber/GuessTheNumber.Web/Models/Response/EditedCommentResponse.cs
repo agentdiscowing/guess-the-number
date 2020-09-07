c9191c0002ed4bc8eab7ef7326a193e4ead70e7b
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-07 18:44:52 +0300

    added response model for edited comment

---
+++ GuessTheNumber/GuessTheNumber.Web/Models/Response/EditedCommentResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessTheNumber.Web.Models.Response
+{
+    public class EditedCommentResponse
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
