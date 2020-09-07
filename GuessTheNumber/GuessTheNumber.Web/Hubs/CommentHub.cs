3ea092b49d8999e4ca932d95306e43ea88abd18a
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-07 18:44:16 +0300

    Added strongly typed hub

---
+++ GuessTheNumber/GuessTheNumber.Web/Hubs/CommentHub.cs
@@ -0,0 +1,15 @@
+﻿namespace GuessTheNumber.Web.Hubs
+{
+    using GuessTheNumber.Web.Interfaces;
+    using GuessTheNumber.Web.Models.Response;
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class CommentHub : Hub<ICommentClient>
+    {
+        public Task NewCommentAdded(CommentResponse newComment)
+        {
+            return this.Clients.Others.NewCommentAdded(newComment);
+        }
+    }
+}
---
+++ GuessTheNumber/GuessTheNumber.Web/Interfaces/ICommentClient.cs
@@ -0,0 +1,14 @@
+﻿namespace GuessTheNumber.Web.Interfaces
+{
+    using System.Threading.Tasks;
+    using GuessTheNumber.Web.Models.Response;
+
+    public interface ICommentClient
+    {
+        Task NewCommentAdded(CommentResponse newComment);
+
+        Task CommentModified(EditedCommentResponse editedComment);
+
+        Task CommentDeleted(int commentId);
+    }
+}
