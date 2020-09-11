8301b34d332f2d6c282bebf11058ef050c4324da
Lolita Hevondian <agentdiscowing@gmail.com>
2020-09-11 13:41:00 +0300

    Created refresh token entity

---
+++ GuessTheNumber/GuessTheNumber.DAL/Entities/RefreshToken.cs
@@ -0,0 +1,26 @@
+﻿namespace GuessTheNumber.DAL.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Microsoft.AspNetCore.Identity;
+
+        public class RefreshToken
+        {
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public string Token { get; set; }
+
+            public string JwtId { get; set; }
+
+            public DateTime CreationDate { get; set; }
+
+            public DateTime ExpiryDate { get; set; }
+
+            public bool Used { get; set; }
+
+            public bool Invalidated { get; set; }
+
+            public string UserId { get; set; }
+
+            public virtual IdentityUser User { get; set; }
+        }
+}
