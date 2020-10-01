95f49fe242dc471254147e38d01046bd28433b8e
Lolita Hevondian <agentdiscowing@gmail.com>
2020-10-01 15:35:49 +0300

    Added extension for usability

---
+++ GuessTheNumber/GuessTheNumber.Kafka/Extensions/RegisterServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace GuessTheNumber.Kafka.Extensions
+{
+    using System;
+    using GuessTheNumber.Kafka.Consumer;
+    using GuessTheNumber.Kafka.Interfaces;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class RegisterServiceExtensions
+    {
+        public static IServiceCollection AddKafkaConsumer<TKey, TValue, THandler>(this IServiceCollection services, Action<KafkaConsumerConfig> configAction)
+            where THandler : class, IEventHandler<TKey, TValue>
+            where TValue : IEvent
+        {
+            services.AddScoped<IEventHandler<TKey, TValue>, THandler>();
+
+            services.AddHostedService<KafkaConsumer<TKey, TValue>>();
+
+            services.Configure(configAction);
+
+            return services;
+        }
+    }
+}
