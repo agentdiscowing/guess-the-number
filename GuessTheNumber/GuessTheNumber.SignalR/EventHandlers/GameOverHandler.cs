2bee7c9797423d0e8e7d03788ea5b9419943cb57
Lolita Hevondian <agentdiscowing@gmail.com>
2020-10-01 15:43:09 +0300

    Added event handlers

---
+++ GuessTheNumber/GuessTheNumber.SignalR/EventHandlers/GameOverHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace GuessTheNumber.SignalR.EventHandlers
+{
+    using System.Threading.Tasks;
+    using Confluent.Kafka;
+    using GuessTheNumber.Kafka.Interfaces;
+    using GuessTheNumber.SignalR.Events;
+    using GuessTheNumber.SignalR.Hubs;
+    using GuessTheNumber.SignalR.Interfaces;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class GameOverHandler: IEventHandler<Ignore, GameOver>
+    {
+        private readonly IHubContext<GameHub, IGameClient> commentHubContext;
+
+        public GameOverHandler(IHubContext<GameHub, IGameClient> commentHubContext)
+        {
+            this.commentHubContext = commentHubContext;
+        }
+
+        public async Task HandleAsync(Ignore key, GameOver @event)
+        {
+            await this.commentHubContext.Clients.All.SendGameOverMessage();
+        }
+    }
+}
---
+++ GuessTheNumber/GuessTheNumber.SignalR/EventHandlers/GameWonHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace GuessTheNumber.SignalR.EventHandlers
+{
+    using System.Threading.Tasks;
+    using Confluent.Kafka;
+    using GuessTheNumber.Kafka.Interfaces;
+    using GuessTheNumber.SignalR.Events;
+    using GuessTheNumber.SignalR.Hubs;
+    using GuessTheNumber.SignalR.Interfaces;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class GameWonHandler : IEventHandler<Ignore, GameWon>
+    {
+        private readonly IHubContext<GameHub, IGameClient> commentHubContext;
+
+        public GameWonHandler(IHubContext<GameHub, IGameClient> commentHubContext)
+        {
+            this.commentHubContext = commentHubContext;
+        }
+
+        public async Task HandleAsync(Ignore key, GameWon @event)
+        {
+            await this.commentHubContext.Clients.All.SendGameWonMessage(@event.WinnerUsername);
+        }
+    }
+}
