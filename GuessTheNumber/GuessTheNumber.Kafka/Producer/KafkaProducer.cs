5c5fc7954798d2b658c2f91b3d74939cecf3ccfb
Lolita Hevondian <agentdiscowing@gmail.com>
2020-10-01 15:33:43 +0300

    Added Producer wrapper

---
+++ GuessTheNumber/GuessTheNumber.Kafka/Producer/KafkaProducer.cs
@@ -0,0 +1,22 @@
+﻿namespace GuessTheNumber.Kafka.Producer
+{
+    using Confluent.Kafka;
+
+    public class KafkaProducer
+    {
+        private readonly ProducerConfig config;
+
+        public KafkaProducer(ProducerConfig config)
+        {
+            this.config = config;
+        }
+
+        public void Produce<TKey, TValue>(string topic, TKey key, TValue val, ISerializer<TValue> serializer)
+        {
+            using (var producer = new ProducerBuilder<TKey, TValue>(this.config).SetValueSerializer(serializer).Build())
+            {
+                producer.ProduceAsync(topic, new Message<TKey, TValue> { Value = val, Key = key }).Wait();
+            }
+        }
+    }
+}
