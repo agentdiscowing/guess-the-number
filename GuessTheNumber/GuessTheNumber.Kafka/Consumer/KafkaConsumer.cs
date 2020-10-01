e024fdd87ac852e90d771f4a694f6eb1b587ec91
Lolita Hevondian <agentdiscowing@gmail.com>
2020-10-01 15:35:25 +0300

    Added consumer wrapper

---
+++ GuessTheNumber/GuessTheNumber.Kafka/Consumer/KafkaConsumer.cs
@@ -0,0 +1,53 @@
+﻿namespace GuessTheNumber.Kafka.Consumer
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Confluent.Kafka;
+    using GuessTheNumber.Kafka.Interfaces;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Options;
+
+    public class KafkaConsumer<TKey, TValue> : BackgroundService
+        where TValue : IEvent
+    {
+        private readonly KafkaConsumerConfig config;
+        private readonly IServiceScopeFactory serviceScopeFactory;
+        private IEventHandler<TKey, TValue> handler;
+
+        public KafkaConsumer(IOptions<KafkaConsumerConfig> config, IServiceScopeFactory serviceScopeFactory)
+        {
+            this.serviceScopeFactory = serviceScopeFactory;
+            this.config = config.Value;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (var scope = this.serviceScopeFactory.CreateScope())
+            {
+                this.handler = scope.ServiceProvider.GetRequiredService<IEventHandler<TKey, TValue>>();
+
+                var builder = new ConsumerBuilder<TKey, TValue>(this.config).SetValueDeserializer(new KafkaSerializer<TValue>());
+
+                using (IConsumer<TKey, TValue> consumer = builder.Build())
+                {
+                    consumer.Subscribe(this.config.Topic);
+
+                    while (!stoppingToken.IsCancellationRequested)
+                    {
+                        var result = consumer.Consume();
+
+                        if (result != null)
+                        {
+                            await this.handler.HandleAsync(result.Message.Key, result.Message.Value);
+
+                            consumer.Commit(result);
+
+                            consumer.StoreOffset(result);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GuessTheNumber/GuessTheNumber.Kafka/Consumer/KafkaConsumerConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace GuessTheNumber.Kafka.Consumer
+{
+    using Confluent.Kafka;
+
+    public class KafkaConsumerConfig : ConsumerConfig
+    {
+        public KafkaConsumerConfig()
+        {
+            this.AutoOffsetReset = Confluent.Kafka.AutoOffsetReset.Earliest;
+            this.EnableAutoOffsetStore = false;
+        }
+
+        public string Topic { get; set; }
+    }
+}
